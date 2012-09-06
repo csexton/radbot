5a7e79d9ea7aa7eb3648a07e998db4a0f9552ff8
Christopher Sexton <csexton@gmail.com>
2012-09-05 21:50:38 -0400

    Add mongo logging support

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'cinch'
+gem 'bson_ext'
+gem 'mongo'
+gem 'debugger'
--- bot.rb
+++ bot.rb
@@ -1,6 +1,12 @@
 #! /usr/bin/env ruby
 
+require 'uri'
 require 'cinch'
+require 'mongo'
+
+MONGO_URI = ENV['MONGOLAB_URI'] || "mongodb://localhost:27017/radbot_development"
+$conn = Mongo::Connection.from_uri(MONGO_URI)
+$db   = $conn.db(URI.parse(MONGO_URI).path.gsub(/^\//, ''))
 
 $bot = Cinch::Bot.new do
   configure do |c|
@@ -9,6 +15,11 @@ $bot = Cinch::Bot.new do
     c.nick = 'radbot'
     c.realname = 'RadBot'
     c.channels = ['#radius']
+  end
+
+  on :message do |m|
+    # Log all the messages to mongo
+    $db["channel_#{channel}"].insert({'user' => user, 'message' => message, 'time' => Time.now})
   end
 
   on :message, "poke" do |m|
