8233c0e653df7118b814e1ac45a03f9ccd155029
Christopher Sexton <csexton@gmail.com>
2012-09-25 16:50:25 -0400

    Added isup and illogical plugins

--- bot.rb
+++ bot.rb
@@ -122,6 +122,40 @@ $bot = Cinch::Bot.new do
       m.reply "I hope that makes #{n} feel better"
     end
   end
+
+  #
+  # Illogical
+  #
+  on :message, /.*(illogical).*/i do |m|
+    message = [
+      "http://www.katzy.dsl.pipex.com/Smileys/illogical.gif",
+      "http://icanhascheezburger.files.wordpress.com/2010/08/e95f76c6-469b-486e-9d18-b2c600ff7ab6.jpg",
+      "http://fc01.deviantart.net/fs46/i/2009/191/d/6/Spock_Finds_You_Illogical_by_densethemoose.jpg",
+      "http://cache.io9.com/assets/images/8/2008/11/medium_vulcan-cat-is-logical.jpg",
+      "http://roflrazzi.files.wordpress.com/2011/01/funny-celebrity-pictures-karaoke.jpg",
+      "http://i13.photobucket.com/albums/a292/macota/MCCOYGOBLET.jpg",
+      "http://spike.mtvnimages.com/images/import/blog//1/8/7/5/1875583/200905/1242167094687.jpg",
+      "http://randomoverload.com/wp-content/uploads/2010/12/fc5558bae4issors.jpg.jpg"
+    ]
+    m.reply message.sample
+  end
+
+
+  #
+  # Is up plugin
+  #
+  on :message, /is (.*?) (up|down)(\?)?/i do |m, domain|
+    body = open("http://www.isup.me/"+domain).read
+    if body.include? "It's just you"
+      m.reply "#{domain} looks UP from here."
+    elsif body.include? "It's not just you!"
+      m.reply "#{domain} looks DOWN from here."
+    else
+      m.reply  "Not sure, #{domain} returned an error."
+    end
+  end
+
+
 end
 
 $bot.start
